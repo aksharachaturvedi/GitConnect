529a078545d9257ccf5cee2948d9536147d172d8
Akshara <zenachaturvedi@users.noreply.github.com>
2016-02-18 16:00:43 -0800

    added configurations

--- batch/pythonjob/user_edgeids.py
+++ batch/pythonjob/user_edgeids.py
@@ -3,13 +3,14 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 
 # getting master node's IP and public DNS to run Spark job and read from HDFS
-namenode_path = "hdfs://52.89.26.208:9000"
+master_ip = os.environ['master_ip']  
+namenode_path = os.environ['name_node']
+
 file_path = "/data/github/hourly/events/*.json"
 edgePath = "/data/graphPersist/UserRepoGraph/edgeFile/"
-
 SparkContext.setSystemProperty('spark.executor.memory', '4g')
 
-sc = SparkContext("spark://ip-172-31-0-75:7077", "eventRepoUser")
+sc = SparkContext("spark://"+master_ip +":7077", "eventRepoUser")
 sqlContext = SQLContext(sc)
 
 df_events = sqlContext.read.json(namenode_path + file_path)
@@ -19,10 +20,5 @@ event_map = df_events.filter("type = 'PushEvent'")
 repo_rdd = event_map.map(lambda x: (x.repo.name, list([x.actor.id]))).groupByKey()
 
 # get repo and extract the list of users for that repo
-#usr_repo = repo_rdd.map(lambda x: filter(None,itertools.combinations(list(set([repo for sublist in x[1] for repo in sublist])),2)))
-
-usr_repo = repo_rdd.map(lambda x: filter(None,itertools.combinations(map(str,list(set([repo for sublist in x[1] for repo in sublist]))),2)))
-
 usr_repo = repo_rdd.map(lambda x: filter(None,itertools.combinations(filter(None,map(str,list(set([repo for sublist in x[1] for repo in sublist]))),2))))
-
 usr_repo.saveAsTextFile(namenode_path + edgePath)
